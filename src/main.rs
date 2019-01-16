db37494c71ff46892953ea3ba3c11d43eca62875
Bryce Murphy <brshmurphy@gmail.com>
2019-01-16 17:49:13 -0500

    even shittier cli

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,29 @@ use std::collections::HashMap;
 
 mod linter;
 
+// represents a connected user
+struct User {
+    name: Option<String>,
+    password: Option<String>,
+    data: Option<HashMap<String, linter::Entry>> // probably inefficient and dumb
+}
+
 fn main() {
 
+    // block needs to be in this function - possibly for scope issues
     let tcp = TcpStream::connect("pi.theshmurph.com:22").unwrap();
     let mut sess = Session::new().unwrap();
     sess.handshake(&tcp).unwrap();
-    sess.userauth_password("main", "BRMurphy35").unwrap();
+    sess.userauth_password("main", "BRMurphy35").unwrap(); // is possibly bad practice
 
     if sess.authenticated() {
         println!("session connected!");
-        println!("enter user token: ");
-
+        println!("give a command! 'help' for list of commands");
+        loop {
+            let mut temp = String::new();
+            parse_command_gen(&sess, read_next(&mut temp)); // currentsession is mutable because of the login
+        }
+        /*
         let mut t = String::new();
         let token = read_next(&mut t)[0];
         let data: HashMap<String, linter::Entry>; // probably inefficient and dumb
@@ -33,7 +45,7 @@ fn main() {
             }
         } else {
             println!("user does not exist.");
-        }
+        }*/
     } else {
         println!("unable to connect. terminating...")
     }
@@ -44,8 +56,51 @@ fn read_next(buf: &mut String) -> Vec<&str> {
     stdin().read_line(buf).unwrap();
     buf.trim().split(" ").collect()
 }
+
+fn parse_command_gen(session: &Session, command: Vec<&str>) {
+    match command[0] {
+        "help" => help(),
+        "login" => login(session, command[1]),
+        "create" => println!("unimplemented user creation. sorry! :("),
+        "delete" => println!("unimplemented user deletion. sorry! :("),
+        "exit" => std::process::exit(0),
+        _ => println!("unknown command '{}'", command.join(" "))
+    }
+}
+
+fn help() {
+    print!("\nVALID COMMANDS:
+    'login'
+    'create'
+    'delete'
+    'exit'\n");
+}
+
+fn login(session: &Session, user: &str) {
+    if exists(session, user) { // no security thus far for login, add please
+        println!("welcome {}!", &user);
+        start_user(&session, user)
+    } else {
+        println!("user does not exist. would you like to create that user?");
+    }
+}
+
+// could very well be replaced by get_data_for having a little more logic
+fn exists(session: &Session, user: &str) -> bool {
+    let mut channel = session.channel_session().unwrap();
+    channel.exec("cat directory/users.txt").unwrap();
+    let mut s = String::new();
+    channel.read_to_string(&mut s).unwrap();
+    let a: Vec<&str> = s.trim().split('\n').collect();
+    return a.contains(&user)
+}
+
+fn start_user(session: &Session, user: &str) {
+    let data = get_data_for(session, user);
+}
+
 // return data for valid user, or fails
-fn get_data_for(sess: &Session, token: &str) -> HashMap<String, linter::Entry> {
+fn get_data_for(sess: &Session, user: &str) -> HashMap<String, linter::Entry> {
     let mut channel = sess.channel_session().unwrap();
     channel.exec("cat directory/pass.txt").unwrap();
     let mut s = String::new();
@@ -53,19 +108,11 @@ fn get_data_for(sess: &Session, token: &str) -> HashMap<String, linter::Entry> {
     linter::map(s.to_string())
 }
 
-// could very well be replaced by get_data_for having a little more logic
-fn exists(sess: &Session, token: &str) -> bool {
-    let mut channel = sess.channel_session().unwrap();
-    channel.exec("cat directory/users.txt").unwrap();
-    let mut s = String::new();
-    channel.read_to_string(&mut s).unwrap();
-    let a: Vec<&str> = s.trim().split('\n').collect();
-    return a.contains(&token)
-}
-
-fn parse_command(command: Vec<&str>, data: &HashMap<String, linter::Entry>) {
+fn parse_command_user(command: Vec<&str>, data: &HashMap<String, linter::Entry>) {
     match command[0] {
         "find" => find(command[1], data), // try to make OO if it makes sense // also, extend funtionality to include multiple searches per command (not just limited to command[1])
+        "insert" => println!("unimplemented entry insertion. sorry! :("),
+        "remove" => println!("unimplemented entry remove. sorry! :("),
         "exit" => std::process::exit(0),
         _ => println!("unknown command '{}'", command.join(" "))
     }
